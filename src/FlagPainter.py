d028236529ee020c42139e5be74efc5d4f5d546a
Erwin Rossen <erwin.rossen@gmail.com>
2015-06-20 23:52:05 +0200

    Flag Painter

---
+++ src/FlagPainter.py
@@ -0,0 +1,273 @@
+'''
+Created on Jun 20, 2015
+
+@author: Erwin Rossen
+'''
+
+from enum import Enum
+import math
+import os.path
+
+from PIL import Image, ImageDraw
+
+
+def assert_coordinate(xy):
+    assert isinstance(xy, tuple), 'Coordinate must be a 2-tuple, is now a {}'.format(type(xy))
+    assert len(xy) == 2, 'Coordinate must be a 2-tuple'
+    assert xy[0] is not None
+    assert xy[1] is not None
+
+class StripeDirection(Enum):
+    horizontal = 1
+    vertical = 2
+
+class FlagPainter(object):
+
+    def __init__(self, HWratio):
+        '''Create an empty (white) image
+        
+        Inputs:
+        -------
+        HWratio: height-to-width ratio of the flag
+        
+        Sets:
+        -----
+        img: PIL Image, size (W,H). W = 600 pixels, H = HWratio * 600 pixels
+        draw: ImageDraw object, operating self.img
+        '''
+        
+        W = 1024  # pixels
+        H = (int) (W * HWratio)
+        self.img = Image.new("RGBA", (W, H), "white")
+        self.draw = ImageDraw.Draw(self.img)
+    
+    @property
+    def size(self):
+        return self.img.size
+
+    @property
+    def W(self):
+        return self.size[0]
+    
+    @property
+    def H(self):
+        return self.size[1]
+
+    def background(self, color):
+        '''Fill the flag with a background color)'''
+        self.draw_rectangle((0, 0, 1, 1), color)
+    
+    def stripes(self, colors, ratios, stripe_direction):
+        '''Draw colored stripes in the flag
+        
+        Inputs:
+        -------
+        colors: array of colors (from top to bottom or from left to right)
+        ratios: array of ratio of width of stripes (e.g. 2:1:2 = [2,1,2])
+        stripe_direction: member of enum StripeDirection
+        '''
+        
+        assert len(colors) == len(ratios), 'Each stripe shall have a color and a ratio'
+        
+        # Initialize the coordinates
+        if stripe_direction is StripeDirection.horizontal:
+            x_l = 0
+            x_r = self.W
+            y_t = 0
+            y_b = 0
+        elif stripe_direction is StripeDirection.vertical:
+            x_l = 0
+            x_r = 0
+            y_t = 0
+            y_b = self.H
+            
+        for c, r in zip(colors, ratios):
+            # Update the coordinates
+            if stripe_direction is StripeDirection.horizontal:
+                y_t = y_b  # New top is previous bottom
+                y_b += (r / sum(ratios)) * self.H
+            elif stripe_direction is StripeDirection.vertical:
+                x_l = x_r  # New left is previous right
+                x_r += (r / sum(ratios)) * self.W
+            
+            # Draw the rectangle
+            self.draw.rectangle([x_l, y_t, x_r, y_b], fill=c)
+    
+    def draw_horizontal_band(self, height, color):
+        '''Draw a colored horizontal band on top of the curent flag
+        
+        Inputs:
+        -------
+        height: 2-tuple, indicating the relative upper and lower limits of the band
+        color: color of the band to draw
+        '''
+        
+        assert isinstance(height, tuple), 'Height must be a 2-tuple, is now a {}'.format(type(height))
+        assert len(height) == 2, 'Height must be a 2-tuple, is now a {}-tuple'.format(len(height))
+        for i in height:
+            assert i is not None
+        
+        self.draw_rectangle((0, height[0], 1, height[1]), color)
+        
+    def draw_vertical_band(self, width, color):
+        '''Draw a colored vertical band on top of the curent flag
+        
+        Inputs:
+        -------
+        height: 2-tuple, indicating the relative upper and lower limits of the band
+        color: color of the band to draw
+        '''
+        
+        assert isinstance(width, tuple), 'Width must be a 2-tuple, is now a {}'.format(type(width))
+        assert len(width) == 2, 'Width must be a 2-tuple, is now a {}-tuple'.format(len(width))
+        for i in width:
+            assert i is not None
+        
+        self.draw_rectangle((width[0], 0, width[1], 1), color)
+    
+    def draw_rectangle(self, box, color):
+        '''Draw a colored box on top of the current flag
+        
+        Inputs:
+        -------
+        box: 4-tuple, indicating relative position of (left, upper, right, lower) box
+        color: color of the rectangle to draw
+        '''
+        
+        assert isinstance(box, tuple), 'Box must be a 4-tuple, is now a {}'.format(type(box))
+        assert len(box) == 4, 'Box must be a 4-tuple, is now a {}-tuple'.format(len(box))
+        for i in box:
+            assert i is not None
+        
+        left = box[0] * self.W
+        right = box[2] * self.W
+        upper = box[1] * self.H
+        lower = box[3] * self.H
+        self.draw.rectangle([left, upper, right, lower], fill=color, outline=None)
+    
+    def draw_polygon(self, points, color):
+        '''Draw a colored polygon on top of the current flag
+        
+        Inputs:
+        -------
+        points: Array of relative coordinates of the polygon nodes
+                It should contain at least three coordinates
+                Each coordinate shall be a 2-tuple (X,Y)
+        color: color of the polygon to draw
+        '''
+        
+        points_abs = list()
+        assert len(points) >= 3, 'There shall be at least three coordinates'
+        for xy in points:
+            points_abs.append((int(round(xy[0] * self.W)), int(round(xy[1] * self.H))))
+        
+        self.draw.polygon(points_abs, fill=color, outline=None)
+    
+    def draw_circle(self, center, radius, color):
+        '''Draw a colored circle on top of the current flag
+        
+        Inputs:
+        -------
+        center: Relative coordinate of the center of the circle (w.r.t. size of flag)
+                    center = (X,Y)
+        radius: Relative radius of the circle (w.r.t. width!!)
+        color: color of the circle to draw
+        '''    
+        
+        assert_coordinate(center)
+        assert radius is not None
+        
+        left = int(round((center[0] - radius) * self.W))
+        right = int(round((center[0] + radius) * self.W))
+        upper = int(round(center[1] * self.H - radius * self.W))
+        lower = int(round(center[1] * self.H + radius * self.W))
+        self.draw.ellipse((left, upper, right, lower), fill=color, outline=None)
+    
+    def draw_star(self, center, radius_inner, radius_outer, N_points, starting_alpha, color):
+        '''Draw a star on top of the current flag
+        
+        Inputs:
+        -------
+        center: relative center of the star (w.r.t. to the flag's size)
+        radius_inner: relative radius of the inner circle (w.r.t. the flag's width!!)
+        radius_outer: relative radius of star (w.r.t. the flag's width!!)
+        N_points: number of points the star has. Shall be at least 3
+        starting_alpha: rotation angle in radians
+        color: color of the star to draw
+        '''
+        
+        assert_coordinate(center)
+        assert radius_inner > 0, 'inner radius shall be larger than 0'
+        assert radius_outer > 0, 'outer radius shall be larger than 0'
+        assert radius_outer > radius_inner, 'outer radius shall be larger than inner radius'
+        assert N_points >= 3, 'Number of points shall be at least 3'
+        
+        self.draw_circle(center, radius_inner, color)
+        X = center[0]
+        Y = center[1]
+        R = radius_outer
+        r = radius_inner
+        HW = self.H / self.W
+        Delta_alpha = math.pi / N_points
+        for p in range(N_points):
+            alpha = 2 * math.pi * p / N_points + starting_alpha
+            X1 = X + R * math.cos(alpha)
+            Y1 = Y + R * math.sin(alpha) / HW
+            X2 = X + r * math.cos(alpha + Delta_alpha)
+            Y2 = Y + r * math.sin(alpha + Delta_alpha) / HW
+            X3 = X + r * math.cos(alpha - Delta_alpha)
+            Y3 = Y + r * math.sin(alpha - Delta_alpha) / HW
+            self.draw_polygon([(X1, Y1), (X2, Y2), (X3, Y3)], color)
+    
+    def place_drawing(self, img_name, center, size):
+        '''Place a drawing on the flag
+        
+        Inputs:
+        -------
+        img_name: string with the name of the file which holds the drawing
+        center: 2-tuple, giving the relative position (w.r.t. flag size) of the center of the drawing.
+                center = (X,Y)
+        size: 2-tuple, indicating the relative size  (w.r.t. flag size) of the drawing.
+              size = (W,H)
+              If W is None and H is not None, the width is determined from H and the drawing size
+              If H is None and W is not None, the height is determined from W and the drawing size
+              If W and H are not None, the width and height are determined from the drawing size (not recommended)
+        '''
+        
+        assert_coordinate(center)
+        
+        assert isinstance(size, tuple), 'Size must be a 2-tuple, is now a {}'.format(type(center))
+        assert len(size) == 2, 'Size must be a 2-tuple'
+        
+        pkg_dir = os.path.dirname(__file__)
+        img_dir = os.path.join(pkg_dir, '..', 'img', 'flag_drawings')
+        full_img_path = os.path.join(img_dir, img_name)
+        drawing = Image.open(full_img_path)
+        
+        # Determine the size
+        if size[0] is not None:
+            W = size[0] * self.W
+        if size[1] is not None:
+            H = size[1] * self.H
+        if size[0] is None:
+            W = (drawing.size[0] / drawing.size[1]) * H
+        if size[1] is None:
+            H = (drawing.size[1] / drawing.size[0]) * W
+        drawing = drawing.resize((int(round(W)), int(round(H))), Image.ANTIALIAS)
+        
+        # Determine the placement position
+        X_center = center[0] * self.W
+        Y_center = center[1] * self.H
+        left = int(round(X_center - W / 2))
+        upper = int(round(Y_center - H / 2))
+        
+        # Place the drawing
+        # The third argument is the mask. Without it, the drawing as a white background.
+        self.img.paste(drawing, (left, upper), drawing)
+    
+    def save(self, img_name, img_dir=None):
+        if img_dir is None:
+            pkg_dir = os.path.dirname(__file__)
+            img_dir = os.path.join(pkg_dir, '..', 'img', 'flags')
+        full_img_path = os.path.join(img_dir, '{}.png'.format(img_name))
+        self.img.save(full_img_path, quality=95, optimize=True)
---
+++ src/FlagRecipes.py
@@ -0,0 +1,384 @@
+'''
+Created on Jun 20, 2015
+
+@author: Erwin Rossen
+'''
+
+import math
+import os.path
+
+from PIL import Image, ImageChops
+from django.utils.termcolors import background
+
+from FlagPainter import FlagPainter, StripeDirection
+
+
+def paint_flag_albania():
+    f = FlagPainter(5 / 7)
+    color = (213, 39, 34)
+    f.background(color)
+    f.place_drawing('albania_coat_of_arms.png', (1 / 2, 1 / 2), (18 / 48, 20 / 30))
+    f.save('albania')
+
+def paint_flag_andorra():
+    f = FlagPainter(7 / 10)
+    colors = [(28, 63, 148), (255, 238, 0), (237, 22, 79)]
+    ratios = [64, 72, 64]
+    f.stripes(colors, ratios, StripeDirection.vertical)
+    f.place_drawing('andorra_coat_of_arms.png', (1 / 2, 1 / 2), (62 / 200, 66 / 140))
+    f.save('andorra')
+
+def paint_flag_armenia():
+    f = FlagPainter(13 / 15)
+    colors = [(217, 0, 18), (0, 51, 160), (242, 168, 0)]
+    ratios = [1, 1, 1]
+    f.stripes(colors, ratios, StripeDirection.vertical)
+    f.save('armenia')
+
+def paint_flag_austria():
+    f = FlagPainter(2 / 3)
+    colors = [(197, 5, 50), (255, 255, 255), (197, 5, 50)]
+    ratios = [1, 1, 1]
+    f.stripes(colors, ratios, StripeDirection.horizontal)
+    f.save('austria')
+        
+def paint_flag_azerbaijan():
+    f = FlagPainter(1 / 2)
+    colors = [(0, 151, 195), (224, 0, 52), (0, 174, 101)]
+    drawing_color = (255, 255, 255)
+    ratios = [1, 1, 1]
+    f.stripes(colors, ratios, StripeDirection.horizontal)
+    f.draw_circle((114 / 240, 1 / 2), 18 / 240, drawing_color)
+    f.draw_circle((1 / 2, 1 / 2), 15 / 240, colors[1])
+    f.draw_star(center=(136 / 240, 1 / 2), radius_inner=5 / 240, radius_outer=10 / 240,
+                N_points=8, starting_alpha=0, color=drawing_color)
+    f.save('azerbaijan')
+
+def paint_flag_belarus():
+    f = FlagPainter(1 / 2)
+    colors = [(213, 39, 34), (46, 174, 103)]
+    white = (255, 255, 255)
+    ratios = [2, 1]
+    f.stripes(colors, ratios, StripeDirection.horizontal)
+    f.draw_vertical_band((0, 70 / 630), white)
+    f.place_drawing('belarus_ornament.png', center=(35 / 630, 1 / 2), size=(70 / 630, 1))
+    f.save('belarus')
+    
+def paint_flag_belgium():
+    f = FlagPainter(13 / 15)
+    colors = [(0, 0, 0), (255, 233, 54), (255, 15, 33)]
+    ratios = [1, 1, 1]
+    f.stripes(colors, ratios, StripeDirection.vertical)
+    f.save('belgium')
+
+def paint_flag_bosnia_herzegovina():
+    f = FlagPainter(1 / 2)
+    background_color = (14, 19, 150)
+    triangle_color = (251, 207, 0)
+    star_color = (255, 255, 255)
+    f.background(background_color)
+    points = [(106 / 400, 0), (306 / 400, 0), (306 / 400, 1)]
+    f.draw_polygon(points, triangle_color)
+    
+    X_start = 60 / 400
+    X_end = 260 / 400
+    Y_start = -5 / 200
+    Y_end = 195 / 200
+    N_stars = 9
+    for i in range(N_stars):
+        X = X_start + (i / (N_stars - 1)) * (X_end - X_start)
+        Y = Y_start + (i / (N_stars - 1)) * (Y_end - Y_start)
+        f.draw_star(center=(X, Y), radius_inner=9 / 400, radius_outer=19 / 400,
+                    N_points=5, starting_alpha=-math.pi / 2, color=star_color)
+        
+    f.save('bosnia_herzegovina')
+
+def paint_flag_bulgaria():
+    f = FlagPainter(3 / 5)
+    colors = [(255, 255, 255), (0, 150, 110), (214, 38, 18)]
+    ratios = [1, 1, 1]
+    f.stripes(colors, ratios, StripeDirection.horizontal)
+    f.save('bulgaria')
+
+def paint_flag_croatia():
+    f = FlagPainter(1 / 2)
+    colors = [(255, 0, 0), (255, 255, 255), (23, 61, 174)]
+    ratios = [1, 1, 1]
+    f.stripes(colors, ratios, StripeDirection.horizontal)
+    W = 6 / 5 * 10 / 48
+    H = 795 / 600 * W / (f.H / f.W)
+    upper = 1 / 3 - 250 / 795 * H
+    lower = 1 / 3 + 545 / 795 * H
+    f.place_drawing('croatia_coat_of_arms.png', center=(1 / 2, (upper + lower) / 2), size=(W, H))
+    f.save('croatia')
+
+def paint_flag_cyprus():
+    '''Flag construction: http://www.vexilla-mundi.com/cyprus_flag.html'''
+    f = FlagPainter(2 / 3)
+    white = (255, 255, 255)
+    f.background(white)
+    f.place_drawing('cyprus_detail.png', center=(9.5 / 18, 5.75 / 12), size=(10 / 18, 8.5 / 12))
+    f.save('cyprus')
+
+def paint_flag_czech_republic():
+    f = FlagPainter(2 / 3)
+    colors = [(255, 255, 255), (224, 0, 54)]
+    blue = (23, 77, 148)
+    ratios = [1, 1]
+    f.stripes(colors, ratios, StripeDirection.horizontal)
+    points = [(0, 0), (1 / 2, 1 / 2), (0, 1)]
+    f.draw_polygon(points, blue)
+    f.save('czech_republic')
+
+def paint_flag_denmark():
+    '''Flag construction: http://www.crwflags.com/fotw/flags/dk.html'''
+    f = FlagPainter(14 / 17)
+    colors = [(216, 30, 5), (255, 255, 255)]
+    f.background(colors[0])
+    f.draw_horizontal_band((6 / 14, 8 / 14), colors[1])
+    f.draw_vertical_band((6 / 17, 8 / 17), colors[1])
+    f.save('denmark')
+
+def paint_flag_estonia():
+    f = FlagPainter(7 / 11)
+    colors = [(36, 109, 208), (0, 0, 0), (255, 255, 255)]
+    ratios = [1, 1, 1]
+    f.stripes(colors, ratios, StripeDirection.horizontal)
+    f.save('estonia')
+
+def paint_flag_faroe():
+    '''Flag construction: http://www.crwflags.com/fotw/flags/fo.html'''
+    f = FlagPainter(16 / 22)
+    colors = [(255, 255, 255), (0, 102, 204), (255, 0, 0)]
+    f.background(colors[0])
+    f.draw_horizontal_band((6 / 16, 10 / 16), colors[1])
+    f.draw_vertical_band((6 / 22, 10 / 22), colors[1])
+    f.draw_horizontal_band((7 / 16, 9 / 16), colors[2])
+    f.draw_vertical_band((7 / 22, 9 / 22), colors[2])
+    f.save('faroe')
+
+def paint_flag_finland():
+    '''Flag construction: http://www.crwflags.com/fotw/flags/fi.html'''
+    f = FlagPainter(11 / 18)
+    colors = [(255, 255, 255), (0, 51, 153)]
+    f.background(colors[0])
+    f.draw_horizontal_band((4 / 11, 7 / 11), colors[1])
+    f.draw_vertical_band((5 / 18, 8 / 18), colors[1])
+    f.save('finland')
+
+def paint_flag_france():
+    f = FlagPainter(2 / 3)
+    colors = [(0, 85, 164), (255, 255, 255), (239, 65, 53)]
+    ratios = [1, 1, 1]
+    f.stripes(colors, ratios, StripeDirection.vertical)
+    f.save('france')
+
+def paint_flag_georgia():
+    '''Flag construction: http://www.vexilla-mundi.com/georgia_flag.html'''
+    f = FlagPainter(2 / 3)
+    
+    background_color = (255, 255, 255)
+    draw_color = (224, 0, 54)
+    f.background(background_color)
+    for X in [45 / 300, 215 / 300]:
+        for Y in [20 / 200, 140 / 200]:
+            f.draw_rectangle((X + 15 / 300, Y, X + 25 / 300, Y + 40 / 200), draw_color)
+            f.draw_rectangle((X, Y + 15 / 200, X + 40 / 300, Y + 25 / 200), draw_color)
+    f.draw_horizontal_band(height=(80 / 200, 120 / 200), color=draw_color)
+    f.draw_vertical_band(width=(130 / 300, 170 / 300), color=draw_color)
+    f.save('georgia')
+    
+def paint_flag_germany():
+    f = FlagPainter(3 / 5)
+    colors = [(0, 0, 0), (255, 0, 0), (255, 204, 0)]
+    ratios = [1, 1, 1]
+    f.stripes(colors, ratios, StripeDirection.horizontal)
+    f.save('germany')
+
+def paint_flag_iceland():
+    '''Flag construction: http://www.crwflags.com/fotw/flags/is.html'''
+    f = FlagPainter(18 / 25)
+    colors = [(0, 0, 204), (255, 255, 255), (255, 0, 0)]
+    f.background(colors[0])
+    f.draw_horizontal_band((7 / 18, 11 / 18), colors[1])
+    f.draw_vertical_band((7 / 25, 11 / 25), colors[1])
+    f.draw_horizontal_band((8 / 18, 10 / 18), colors[2])
+    f.draw_vertical_band((8 / 25, 10 / 25), colors[2])
+    f.save('iceland')
+
+def paint_flag_ireland():
+    f = FlagPainter(1 / 2)
+    colors = [(0, 154, 68), (255, 255, 255), (255, 130, 0)]
+    ratios = [1, 1, 1]
+    f.stripes(colors, ratios, StripeDirection.vertical)
+    f.save('ireland')
+
+def paint_flag_italy():
+    f = FlagPainter(2 / 3)
+    colors = [(0, 146, 70), (255, 255, 255), (206, 43, 55)]
+    ratios = [1, 1, 1]
+    f.stripes(colors, ratios, StripeDirection.vertical)
+    f.save('italy')
+    
+def paint_flag_luxembourg():
+    f = FlagPainter(3 / 5)
+    colors = [(273, 41, 57), (255, 255, 255), (0, 161, 222)]
+    ratios = [1, 1, 1]
+    f.stripes(colors, ratios, StripeDirection.horizontal)
+    f.save('luxembourg')
+    
+def paint_flag_netherlands():
+    f = FlagPainter(2 / 3)
+    colors = [(174, 28, 40), (255, 255, 255), (33, 70, 139)]
+    ratios = [1, 1, 1]
+    f.stripes(colors, ratios, StripeDirection.horizontal)
+    f.save('netherlands')
+
+def paint_flag_norway():
+    '''Flag construction: http://www.crwflags.com/fotw/flags/no.html'''
+    f = FlagPainter(16 / 22)
+    colors = [(255, 0, 0), (255, 255, 255), (0, 51, 102)]
+    f.background(colors[0])
+    f.draw_horizontal_band((6 / 16, 10 / 16), colors[1])
+    f.draw_vertical_band((6 / 22, 10 / 22), colors[1])
+    f.draw_horizontal_band((7 / 16, 9 / 16), colors[2])
+    f.draw_vertical_band((7 / 22, 9 / 22), colors[2])
+    f.save('norway')
+
+def paint_flag_portugal():
+    f = FlagPainter(2 / 3)
+    colors = [(255, 0, 0), (0, 102, 0)]
+    ratios = [2, 3]
+    f.stripes(colors, ratios, StripeDirection.vertical)
+    f.place_drawing('portugal_coat_of_arms.png', (2 / 5, 1 / 2), (None, 1 / 2))
+    f.save('portugal')
+    
+def paint_flag_spain():
+    f = FlagPainter(2 / 3)
+    colors = [(170, 21, 27), (241, 191, 0), (170, 21, 27)]
+    ratios = [1, 2, 1]
+    f.stripes(colors, ratios, StripeDirection.horizontal)
+    f.place_drawing('spain_coat_of_arms.png', (1 / 3, 1 / 2), (None, 2 / 5))
+    f.save('spain')
+
+def paint_flag_sweden():
+    '''Flag construction: http://www.crwflags.com/fotw/flags/se.html'''
+    f = FlagPainter(10 / 16)
+    colors = [(0, 102, 153), (255, 204, 0)]
+    f.background(colors[0])
+    f.draw_horizontal_band((4 / 10, 6 / 10), colors[1])
+    f.draw_vertical_band((5 / 16, 7 / 16), colors[1])
+    f.save('sweden')
+
+def paint_flag_uk():
+    f = FlagPainter(1 / 2)
+    colors = [(0, 36, 125), (207, 20, 43), (255, 255, 255)]
+
+    # Background color
+    f.background(colors[0])
+    
+    # White diagonal stripes
+    X = (1 / math.cos(math.pi / 3)) / 60
+    Y = (1 / math.cos(math.pi / 6)) / 30
+    points = [(0, 0), (0, 3 * Y), (1 - 3 * X, 1), (1, 1), (1, 1 - 3 * Y), (3 * X, 0)]
+    f.draw_polygon(points, colors[2])
+    points = [(1, 0), (1 - 3 * X, 0), (0, 1 - 3 * Y), (0, 1), (3 * X, 1), (1, 3 * Y)]
+    f.draw_polygon(points, colors[2])
+    
+    # Red diagonal stripes
+    points = [(0, 0), (0, 2 * Y), ((20 / 60) - 2 * X, 10 / 30), (20 / 60, 10 / 30)]
+    f.draw_polygon(points, colors[1])
+    points = [(1, 0), (1 - 2 * X, 0), (1 - (20 / 60) - 2 * X, 10 / 30), (1 - (20 / 60), 10 / 30)]
+    f.draw_polygon(points, colors[1])
+    points = [(1, 1), (1, 1 - 2 * Y), (1 - (20 / 60) + 2 * X, 1 - (10 / 30)), (1 - (20 / 60), 1 - (10 / 30))]
+    f.draw_polygon(points, colors[1])
+    points = [(0, 1), (2 * X, 1), (20 / 60 + 2 * X, 1 - (10 / 30)), (20 / 60, 1 - (10 / 30))]
+    f.draw_polygon(points, colors[1])
+    
+    # Horizontal and vertical stripes
+    f.draw_horizontal_band((10 / 30, 20 / 30), colors[2])
+    f.draw_vertical_band((25 / 60, 35 / 60), colors[2])
+    f.draw_horizontal_band((12 / 30, 18 / 30), colors[1])
+    f.draw_vertical_band((27 / 60, 33 / 60), colors[1])
+    
+    f.save('uk')
+
+'''
+Helper functions
+'''
+    
+def paint_rounded_bar():
+    f = FlagPainter(3 / 6)
+    background_color = (255, 255, 255)
+    draw_color = (224, 0, 54)
+    f.background((0, 0, 0))
+    f.draw_rectangle((1 / 6, 1 / 3, 5 / 6, 2 / 3), color=draw_color)
+    
+    H = 10
+    R = 56
+    alpha = math.asin(H / (2 * R))
+    X = R * math.cos(alpha)
+    f.draw_circle(center=(1 / 6 - X / 60, 1 / 2), radius=R / 60, color=background_color)
+    f.draw_circle(center=(5 / 6 + X / 60, 1 / 2), radius=R / 60, color=background_color)
+    
+    W = 40
+    R = 104
+    alpha = math.asin(W / (2 * R))
+    Y = R * math.cos(alpha)
+    f.draw_circle(center=(1 / 2, 1 / 3 - Y / 30), radius=R / 60, color=background_color)
+    f.draw_circle(center=(1 / 2, 2 / 3 + Y / 30), radius=R / 60, color=background_color)
+    
+    pkg_dir = os.path.dirname(__file__)
+    flag_detail_dir = os.path.join(pkg_dir, '..', 'img', 'flag_drawings')
+    f.save('rounded_bar_with_bg', flag_detail_dir)
+    trim_drawings('rounded_bar_with_bg.png', 'rounded_bar.png')
+    
+    # TODO: make the white transparent, such that we can make a cross out of it
+
+def trim_drawings(filename_in, filename_out):
+    # Trim the details on the flag
+    src_dir = os.path.dirname(__file__)
+    flag_drawings_dir = os.path.join(src_dir, '..', 'img', 'flag_drawings')
+    full_filename_in = os.path.join(flag_drawings_dir, filename_in)
+    img = Image.open(full_filename_in)
+    bg = Image.new(img.mode, img.size, img.getpixel((0, 0)))
+    diff = ImageChops.difference(img, bg)
+    bbox = diff.getbbox()
+    if bbox:
+        img = img.crop(bbox)
+        full_filename_out = os.path.join(flag_drawings_dir, filename_out)
+        img.save(full_filename_out)
+
+if __name__ == '__main__':
+#     trim_drawings(filename_in='cyprus_flag.png', filename_out='cyprus_detail.png')
+    paint_rounded_bar()
+#     paint_flag_albania()
+#     paint_flag_andorra()
+#     paint_flag_armenia()
+#     paint_flag_austria()
+#     paint_flag_azerbaijan()
+#     paint_flag_belarus()
+#     paint_flag_belgium()
+#     paint_flag_bosnia_herzegovina()
+#     paint_flag_bulgaria()
+#     paint_flag_croatia()
+#     paint_flag_cyprus()
+#     paint_flag_czech_republic()
+#     paint_flag_denmark()
+#     paint_flag_estonia()
+#     paint_flag_faroe()
+#     paint_flag_finland()
+#     paint_flag_france()
+    paint_flag_georgia()
+    
+#     paint_flag_germany()
+#     paint_flag_iceland()
+#     paint_flag_ireland()
+#     paint_flag_italy()
+#     paint_flag_luxembourg()
+#     paint_flag_netherlands()
+#     paint_flag_norway()
+#     paint_flag_portugal()
+#     paint_flag_spain()
+#     paint_flag_sweden()
+#     paint_flag_uk()
