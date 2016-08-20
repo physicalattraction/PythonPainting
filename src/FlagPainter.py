4f4453a029b84192b110a5488f8cd3da2dc6f76d
physicalattraction <erwin.rossen@gmail.com>
2016-08-20 21:26:01 +0200

    Coding style

--- src/FlagPainter.py
+++ src/FlagPainter.py
@@ -1,8 +1,8 @@
-'''
+"""
 Created on Jun 20, 2015
 
 @author: Erwin Rossen
-'''
+"""
 
 from enum import Enum
 import math
@@ -11,142 +11,150 @@ import os.path
 from PIL import Image, ImageDraw, ImageFont
 import PainterUtils
 
+
 def assert_coordinate(xy):
     assert isinstance(xy, tuple), 'Coordinate must be a 2-tuple, is now a {}'.format(type(xy))
     assert len(xy) == 2, 'Coordinate must be a 2-tuple'
     assert xy[0] is not None
     assert xy[1] is not None
 
+
 class StripeDirection(Enum):
     horizontal = 1
     vertical = 2
 
-class FlagPainter(object):
 
-    def __init__(self, HWratio):
-        '''Create an empty (white) image
+class FlagPainter(object):
+    def __init__(self, height_width_ratio):
+        """Create an empty (white) image
         
         Inputs:
         -------
-        HWratio: height-to-width ratio of the flag
+        height_width_ratio: height-to-width ratio of the flag
         
         Sets:
         -----
-        img: PIL Image, size (W,H). W = 600 pixels, H = HWratio * 600 pixels
+        img: PIL Image, size (width,height). width = 600 pixels, height = height_width_ratio * 600 pixels
         draw: ImageDraw object, operating self.img
-        '''
-        
-        W = 1024  # pixels
-        H = (int) (W * HWratio)
-        self.img = Image.new("RGBA", (W, H), "white")
+        """
+
+        width = 1024  # pixels
+        height = (int)(width * height_width_ratio)
+        self.img = Image.new("RGBA", (width, height), "white")
         self.draw = ImageDraw.Draw(self.img)
-    
+
     @property
     def size(self):
         return self.img.size
 
     @property
-    def W(self):
+    def width(self):
         return self.size[0]
-    
+
     @property
-    def H(self):
+    def height(self):
         return self.size[1]
 
     def background(self, color):
-        '''Fill the flag with a background color)'''
+        """Fill the flag with a background color)"""
         self.draw_rectangle((0, 0, 1, 1), color)
-    
+
     def stripes(self, colors, ratios, stripe_direction):
-        '''Draw colored stripes in the flag
+        """Draw colored stripes in the flag
         
         Inputs:
         -------
         colors: array of colors (from top to bottom or from left to right)
         ratios: array of ratio of width of stripes (e.g. 2:1:2 = [2,1,2])
         stripe_direction: member of enum StripeDirection
-        '''
-        
-        assert len(colors) == len(ratios), 'Each stripe shall have a color and a ratio'
-        
+        """
+
+        lc = len(colors)
+        lr = len(ratios)
+        assert lc == lr, 'Each stripe shall have a color and a ratio. Found {} colors and {} ratios'.format(
+            lc, lr)
+
         # Initialize the coordinates
         if stripe_direction is StripeDirection.horizontal:
             x_l = 0
-            x_r = self.W
+            x_r = self.width
             y_t = 0
             y_b = 0
         elif stripe_direction is StripeDirection.vertical:
             x_l = 0
             x_r = 0
             y_t = 0
-            y_b = self.H
-            
+            y_b = self.height
+
         for c, r in zip(colors, ratios):
             # Update the coordinates
             if stripe_direction is StripeDirection.horizontal:
                 y_t = y_b  # New top is previous bottom
-                y_b += (r / sum(ratios)) * self.H
+                y_b += (r / sum(ratios)) * self.height
             elif stripe_direction is StripeDirection.vertical:
                 x_l = x_r  # New left is previous right
-                x_r += (r / sum(ratios)) * self.W
-            
+                x_r += (r / sum(ratios)) * self.width
+            else:
+                raise Exception("Stripe direction shall be either horizontal or vertical")
+
             # Draw the rectangle
             self.draw.rectangle([x_l, y_t, x_r, y_b], fill=c)
-    
+
     def draw_horizontal_band(self, height, color):
-        '''Draw a colored horizontal band on top of the curent flag
+        """Draw a colored horizontal band on top of the curent flag
         
         Inputs:
         -------
         height: 2-tuple, indicating the relative upper and lower limits of the band
         color: color of the band to draw
-        '''
-        
-        assert isinstance(height, tuple), 'Height must be a 2-tuple, is now a {}'.format(type(height))
+        """
+
+        assert isinstance(height, tuple), 'Height must be a 2-tuple, is now a {}'.format(
+            type(height))
         assert len(height) == 2, 'Height must be a 2-tuple, is now a {}-tuple'.format(len(height))
         for i in height:
             assert i is not None
-        
+
         self.draw_rectangle((0, height[0], 1, height[1]), color)
-        
+
     def draw_vertical_band(self, width, color):
-        '''Draw a colored vertical band on top of the curent flag
+        """Draw a colored vertical band on top of the curent flag
         
         Inputs:
         -------
         height: 2-tuple, indicating the relative upper and lower limits of the band
         color: color of the band to draw
-        '''
-        
+        """
+
         assert isinstance(width, tuple), 'Width must be a 2-tuple, is now a {}'.format(type(width))
         assert len(width) == 2, 'Width must be a 2-tuple, is now a {}-tuple'.format(len(width))
         for i in width:
             assert i is not None
-        
+
         self.draw_rectangle((width[0], 0, width[1], 1), color)
-    
+
     def draw_rectangle(self, box, color):
-        '''Draw a colored box on top of the current flag
+        """Draw a colored box on top of the current flag
         
         Inputs:
         -------
         box: 4-tuple, indicating relative position of (left, upper, right, lower) box
         color: color of the rectangle to draw
-        '''
-        
+        """
+
         assert isinstance(box, tuple), 'Box must be a 4-tuple, is now a {}'.format(type(box))
         assert len(box) == 4, 'Box must be a 4-tuple, is now a {}-tuple'.format(len(box))
         for i in box:
             assert i is not None
-        
-        left = box[0] * self.W
-        right = box[2] * self.W
-        upper = box[1] * self.H
-        lower = box[3] * self.H
+
+        left = box[0] * self.width
+        right = box[2] * self.width
+        upper = box[1] * self.height
+        lower = box[3] * self.height
         self.draw.rectangle([left, upper, right, lower], fill=color, outline=None)
-    
+
     def draw_polygon(self, points, color):
-        '''Draw a colored polygon on top of the current flag
+        """Draw a colored polygon on top of the current flag
         
         Inputs:
         -------
@@ -154,17 +162,17 @@ class FlagPainter(object):
                 It should contain at least three coordinates
                 Each coordinate shall be a 2-tuple (X,Y)
         color: color of the polygon to draw
-        '''
-        
+        """
+
         points_abs = list()
         assert len(points) >= 3, 'There shall be at least three coordinates'
         for xy in points:
-            points_abs.append((int(round(xy[0] * self.W)), int(round(xy[1] * self.H))))
-        
+            points_abs.append((int(round(xy[0] * self.width)), int(round(xy[1] * self.height))))
+
         self.draw.polygon(points_abs, fill=color, outline=None)
-    
+
     def draw_circle(self, center, radius, color):
-        '''Draw a colored circle on top of the current flag
+        """Draw a colored circle on top of the current flag
         
         Inputs:
         -------
@@ -172,109 +180,114 @@ class FlagPainter(object):
                     center = (X,Y)
         radius: Relative radius of the circle (w.r.t. width!!)
         color: color of the circle to draw
-        '''    
-        
+        """
+
         assert_coordinate(center)
         assert radius is not None
-        
-        left = int(round((center[0] - radius) * self.W))
-        right = int(round((center[0] + radius) * self.W))
-        upper = int(round(center[1] * self.H - radius * self.W))
-        lower = int(round(center[1] * self.H + radius * self.W))
+
+        left = int(round((center[0] - radius) * self.width))
+        right = int(round((center[0] + radius) * self.width))
+        upper = int(round(center[1] * self.height - radius * self.width))
+        lower = int(round(center[1] * self.height + radius * self.width))
         self.draw.ellipse((left, upper, right, lower), fill=color, outline=None)
-    
-    def draw_star(self, center, radius_inner, radius_outer, N_points, starting_alpha, color):
-        '''Draw a star on top of the current flag
+
+    def draw_star(self, center, radius_inner, radius_outer, nr_points, starting_alpha, color):
+        """Draw a star on top of the current flag
         
         Inputs:
         -------
         center: relative center of the star (w.r.t. to the flag's size)
         radius_inner: relative radius of the inner circle (w.r.t. the flag's width!!)
         radius_outer: relative radius of star (w.r.t. the flag's width!!)
-        N_points: number of points the star has. Shall be at least 3
+        nr_points: number of points the star has. Shall be at least 3
         starting_alpha: rotation angle in radians
         color: color of the star to draw
-        '''
-        
+        """
+
         assert_coordinate(center)
         assert radius_inner > 0, 'inner radius shall be larger than 0'
         assert radius_outer > 0, 'outer radius shall be larger than 0'
         assert radius_outer > radius_inner, 'outer radius shall be larger than inner radius'
-        assert N_points >= 3, 'Number of points shall be at least 3'
-        
+        assert nr_points >= 3, 'Number of points shall be at least 3'
+
         self.draw_circle(center, radius_inner, color)
-        X = center[0]
-        Y = center[1]
-        R = radius_outer
-        r = radius_inner
-        HW = self.H / self.W
-        Delta_alpha = math.pi / N_points
-        for p in range(N_points):
-            alpha = 2 * math.pi * p / N_points + starting_alpha
-            X1 = X + R * math.cos(alpha)
-            Y1 = Y + R * math.sin(alpha) / HW
-            X2 = X + r * math.cos(alpha + Delta_alpha)
-            Y2 = Y + r * math.sin(alpha + Delta_alpha) / HW
-            X3 = X + r * math.cos(alpha - Delta_alpha)
-            Y3 = Y + r * math.sin(alpha - Delta_alpha) / HW
-            self.draw_polygon([(X1, Y1), (X2, Y2), (X3, Y3)], color)
-    
+        x = center[0]
+        y = center[1]
+        ro = radius_outer
+        ri = radius_inner
+        height_width_ratio = self.height / self.width
+        Delta_alpha = math.pi / nr_points
+        for p in range(nr_points):
+            alpha = 2 * math.pi * p / nr_points + starting_alpha
+            x1 = x + ro * math.cos(alpha)
+            y1 = y + ro * math.sin(alpha) / height_width_ratio
+            x2 = x + ri * math.cos(alpha + Delta_alpha)
+            y2 = y + ri * math.sin(alpha + Delta_alpha) / height_width_ratio
+            x3 = x + ri * math.cos(alpha - Delta_alpha)
+            y3 = y + ri * math.sin(alpha - Delta_alpha) / height_width_ratio
+            self.draw_polygon([(x1, y1), (x2, y2), (x3, y3)], color)
+
     def draw_text(self, text, center, color, font='../fonts/Trebuchet MS Bold.ttf', font_size=24):
         my_font = ImageFont.truetype(font, font_size)
-        w, h = self.draw.textsize(text, my_font)
-        x = center[0] * self.W - 1 / 2 * w
-        y = center[1] * self.H - 1 / 2 * h
-        
+        width, height = self.draw.textsize(text, my_font)
+        x = center[0] * self.width - 1 / 2 * width
+        y = center[1] * self.height - 1 / 2 * height
+
         print(x, y)
-        
+
         self.draw.text((x, y), text, fill=color, font=my_font)
-    
+
     def place_drawing(self, img_name, center, size):
-        '''Place a drawing on the flag
+        """Place a drawing on the flag
         
         Inputs:
         -------
         img_name: string with the name of the file which holds the drawing
-        center: 2-tuple, giving the relative position (w.r.t. flag size) of the center of the drawing.
+        center: 2-tuple, giving the relative position (width.r.t. flag size) of the center of the drawing.
                 center = (X,Y)
-        size: 2-tuple, indicating the relative size  (w.r.t. flag size) of the drawing.
-              size = (W,H)
-              If W is None and H is not None, the width is determined from H and the drawing size
-              If H is None and W is not None, the height is determined from W and the drawing size
-              If W and H are not None, the width and height are determined from the drawing size (not recommended)
-        '''
-        
+        size: 2-tuple, indicating the relative size  (width.r.t. flag size) of the drawing.
+              size = (width,height)
+              If width is None and height is not None, the width is determined from height and the drawing size
+              If height is None and width is not None, the height is determined from width and the drawing size
+              If width and height are not None, the width and height are determined from the drawing size (not recommended)
+        """
+
         assert_coordinate(center)
-        
+
         assert isinstance(size, tuple), 'Size must be a 2-tuple, is now a {}'.format(type(center))
         assert len(size) == 2, 'Size must be a 2-tuple'
-        
+
         drawing = PainterUtils.read_flag_drawing(img_name)
-        
+
         # Determine the size
         if size[0] is not None:
-            W = size[0] * self.W
+            width = size[0] * self.width
         if size[1] is not None:
-            H = size[1] * self.H
+            height = size[1] * self.height
         if size[0] is None:
-            W = (drawing.size[0] / drawing.size[1]) * H
+            width = (drawing.size[0] / drawing.size[1]) * height
         if size[1] is None:
-            H = (drawing.size[1] / drawing.size[0]) * W
-        drawing = drawing.resize((int(round(W)), int(round(H))), Image.ANTIALIAS)
-        
+            height = (drawing.size[1] / drawing.size[0]) * width
+        drawing = drawing.resize((int(round(width)), int(round(height))), Image.ANTIALIAS)
+
         # Determine the placement position
-        X_center = center[0] * self.W
-        Y_center = center[1] * self.H
-        left = int(round(X_center - W / 2))
-        upper = int(round(Y_center - H / 2))
-        
+        X_center = center[0] * self.width
+        Y_center = center[1] * self.height
+        left = int(round(X_center - width / 2))
+        upper = int(round(Y_center - height / 2))
+
         # Place the drawing
         # The third argument is the mask. Without it, the drawing has a white background.
         self.img.paste(drawing, (left, upper), drawing)
-    
+
     def save(self, img_name, img_dir=None):
         if img_dir is None:
             img_dir = PainterUtils.flags_dir()
-        
+
+        original_size = self.img.size
+        self.img.resize(size=(2 * original_size[0], 2 * original_size[1]),
+                        resample=Image.ANTIALIAS)
+        self.img.resize(size=original_size, resample=Image.ANTIALIAS)
+
         full_img_path = os.path.join(img_dir, PainterUtils.append_default_extension(img_name))
-        self.img.save(full_img_path, quality=95, optimize=True)
+        self.img.save(full_img_path, quality=95, optimize=True, )
--- src/PainterUtils.py
+++ src/PainterUtils.py
@@ -18,12 +18,12 @@ def append_default_extension(filename, default_extension='.png'):
     else:
         return filename + default_extension
 
+
 def drawings_dir():
     '''Determine the flag_drawings directory'''
-    src_dir = os.path.dirname(__file__)
-    flag_drawings_dir = os.path.join(src_dir, '..', 'img', 'flag_drawings')
-    print('Warning: use get_img_dir instead')
+    flag_drawings_dir = get_img_dir('flag_drawings')
     return flag_drawings_dir
+
 
 def get_img_dir(dir_name):
     '''Determine the correct image directory'''
@@ -33,11 +33,13 @@ def get_img_dir(dir_name):
         os.makedirs(folder)
     return folder
 
+
 def flags_dir():
     '''Determine the flags directory'''
     src_dir = os.path.dirname(__file__)
     flags_dir = os.path.join(src_dir, '..', 'img', 'flags')
     return flags_dir
+
 
 def font_dir():
     '''Determine the fonts directory'''
@@ -45,11 +47,13 @@ def font_dir():
     fonts_dir = os.path.join(src_dir, '..', 'img', 'fonts')
     return fonts_dir
 
+
 def fashioncheque_dir():
     '''Determine the flags directory'''
     src_dir = os.path.dirname(__file__)
     fc_dir = os.path.join(src_dir, '..', 'img', 'fashioncheque')
     return fc_dir
+
 
 def read_flag_drawing(filename_in):
     '''Open an image from a file in the flag_drawings directory'''
@@ -57,9 +61,11 @@ def read_flag_drawing(filename_in):
     img = Image.open(full_filename_in)
     return img
 
+
 def write_flag_drawing(img, filename_out):
     '''Write an image to a file in the flag_drawings directory'''
     save_img(img, 'flag_drawings', filename_out)
+
 
 def save_img(img, dirname, filename, cmyk=False):
     '''Save an image to a file in the indicated img directory'''
@@ -73,7 +79,8 @@ def save_img(img, dirname, filename, cmyk=False):
         # Save directly to RGB
         full_filename_out = os.path.join(full_img_dir, append_default_extension(filename))
         img.save(full_filename_out, quality=95, optimize=True)
-    
+
+
 def trim_img(img):
     '''Trim the edges of an image'''
     bg = Image.new(img.mode, img.size, img.getpixel((0, 0)))
@@ -83,20 +90,23 @@ def trim_img(img):
         img = img.crop(bbox)
     return img
 
+
 def rel2abs(rel_coordinate, size):
     '''Transform a relative coordinate to an absolute coordinate'''
     assert isinstance(rel_coordinate, tuple)
     assert len(rel_coordinate) == 2
-    
+
     abs_coordinate = (int(round(rel_coordinate[0] * size[0])),
                       int(round(rel_coordinate[1] * size[1])))
     return abs_coordinate
+
 
 def rel2abs_geom(rel_number, size):
     '''Transform a relative number to an absolute coordinate, using the geometric mean of the size'''
     assert 0 <= rel_number and rel_number <= 1, '{} shall be between 0 and 1'.format(rel_number)
     abs_number = int(round(np.sqrt(size[0] * size[1]) * rel_number))
     return abs_number
+
 
 if __name__ == '__main__':
     pass
