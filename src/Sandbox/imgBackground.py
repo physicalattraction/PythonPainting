bd9cc38552a744ddb6af2b98cb9005ecc7cb2648
physicalattraction <erwin.rossen@gmail.com>
2017-01-23 09:34:09 +0100

    feat: New Sandbox material

--- src/Sandbox/imgBackground.py
+++ src/Sandbox/imgBackground.py
@@ -1,4 +1,8 @@
 from PIL import Image
+import pyscreenshot
 
-img = Image.new('RGB', (300, 200), (228, 150, 150))
-img.show()
+img = Image.new('RGBA', (300, 200), (228, 150, 150, 0))
+img.save('../../img/test.png')
+
+im = pyscreenshot.grab()
+im.show()
