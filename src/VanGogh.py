b3259ffa8c24b22a26ce21b3d1f1fd072b5a4ab5
Erwin Rossen <erwin.rossen@fashion.cloud>
2021-09-18 18:12:54 +0200

    Make Van Gogh work again

--- src/VanGogh.py
+++ src/VanGogh.py
@@ -1,23 +1,23 @@
-'''
+"""
 Created on Apr 17, 2016
 
 @author: Erwin Rossen
-'''
+"""
 
 from PIL import Image, ImageDraw
 import numpy as np
 import os
 
 
-class VanGogh(object):
-    '''
+class VanGogh:
+    """
     Paint like the famous Dutch artist
-    '''
+    """
 
     def __init__(self, grid_size=50):
-        W = 4000
-        H = W
-        self.__init_img((W, H))
+        width = 4000
+        height = width
+        self._init_img((width, height))
         self.grid_size = grid_size
 
     @property
@@ -25,21 +25,22 @@ class VanGogh(object):
         return self.img.size
 
     @property
-    def W(self):
+    def width(self):
         return self.size[0]
 
     @property
-    def H(self):
+    def height(self):
         return self.size[1]
 
     @property
     def center(self):
-        return (0, 0)
+        return 0, 0
 
     def hex_grid(self):
-        '''
+        """
         Return a hexagonal grid
-        '''
+        """
+
         grid = []
         div_factor = self.grid_size / np.sqrt(6)
         ei = np.array([1, 0]) / div_factor
@@ -57,81 +58,88 @@ class VanGogh(object):
             grid.append(x)
         return grid
 
-    def pointillize_img(self, img):
-        new_height = int(round(self.W * img.size[1] / img.size[0]))
-        self.__init_img((self.W, new_height))
+    def pointillize_img(self, img: Image):
+        new_height = int(round(self.width * img.size[1] / img.size[0]))
+        self._init_img((self.width, new_height))
         radius = 1 / (self.grid_size * 1.05)
         for point in self.random_grid():
-            color = self.__get_pixel(img, point)
+            color = self._get_pixel(img, point)
             if color is not None:
-                self.__draw_circle(point, radius=radius, fill_color=color, outline_color=None)
+                self._draw_circle(point, radius=radius, fill_color=color, outline_color=None)
 
     def show(self):
-        '''Show the image to screen.'''
+        """
+        Show the image to screen
+        """
+
         self.img.show()
 
     def save(self, img_name, img_dir=None):
-        '''Save the image to file.'''
+        """Save the image to file."""
         if img_dir is not None:
-            full_img_path = os.path.join(self.__vangogh_dir(), img_dir, '{}.png'.format(img_name))
+            full_img_path = os.path.join(self._vangogh_dir(), img_dir, '{}.png'.format(img_name))
         else:
-            full_img_path = os.path.join(self.__vangogh_dir(), '{}.png'.format(img_name))
+            full_img_path = os.path.join(self._vangogh_dir(), '{}.png'.format(img_name))
         self.img.save(full_img_path, quality=95, optimize=True)
 
     def get_original_images(self):
-        '''Return a list of filenames in the orig directory.'''
-        search_path = os.path.join(self.__vangogh_dir(), 'orig')
+        """
+        Return a list of filenames in the orig directory
+        """
+
+        search_path = os.path.join(self._vangogh_dir(), 'orig')
         list_imgs = [f for f in os.listdir(search_path) if
                      os.path.isfile(os.path.join(search_path, f))]
         return list_imgs
 
     def read_img(self, filename_in):
-        '''Open an image from a file in the vangogh directory'''
-        full_filename_in = os.path.join(self.__vangogh_dir(), 'orig', filename_in)
-        img = Image.open(full_filename_in)
-        return img
+        """
+        Open an image from a file in the vangogh directory
+        """
 
-    '''
-    Private methods
-    '''
+        full_filename_in = os.path.join(self._vangogh_dir(), 'orig', filename_in)
+        return Image.open(full_filename_in)
 
-    def __vangogh_dir(self):
-        '''Determine the Van Gogh directory'''
+    def _vangogh_dir(self):
+        """
+        Determine the Van Gogh directory
+        """
+
         src_dir = os.path.dirname(__file__)
         vangogh_dir = os.path.join(src_dir, '..', 'img', 'vangogh')
         return vangogh_dir
 
-    def __init_img(self, size):
+    def _init_img(self, size):
         self.img = Image.new("RGBA", size, (255, 255, 255))
         self.draw = ImageDraw.Draw(self.img)
 
-    def __get_pixel(self, img, pixel):
-        '''
+    def _get_pixel(self, img, pixel):
+        """
         Returns the pixel value at a given position.
 
         :param img: Image object of which to determine the image
-        :param xy: The coordinate, given as np.array([x, y]), with (x,y) within the square
-                   between (-1,-1), (-1,1), (1,1) and (1,-1).
-        :returns: The pixel value.  If the image is a multi-layer image,
-                  this method returns a tuple.
-        '''
+        :param pixel: The coordinate, given as np.array([x, y]), with (x,y) within the square
+                      between (-1,-1), (-1,1), (1,1) and (1,-1).
+        :returns: The pixel value.  If the image is a multi-layer image, this method returns a tuple.
+        """
+
         if img is None:
             return None
-        W = img.size[0]
-        H = img.size[1]
-        x = int(round((pixel[0] + 0.5) * W))
-        y = int(round((pixel[1] + 0.5) * H))
-        if (x < 0 or x >= W or y < 0 or y >= H):
+        width = img.size[0]
+        height = img.size[1]
+        x = int(round((pixel[0] + 0.5) * width))
+        y = int(round((pixel[1] + 0.5) * height))
+        if (x < 0 or x >= width or y < 0 or y >= height):
             return None
         return img.getpixel((x, y))
 
-    def __draw_circle(self, center, radius, fill_color, outline_color):
-        '''Draw a circle.'''
-        left = int(round((center[0] - radius + 0.5) * self.W))
-        right = int(round((center[0] + radius + 0.5) * self.W))
-        upper = int(round((center[1] + 0.5) * self.H - radius * self.W))
-        lower = int(round((center[1] + 0.5) * self.H + radius * self.W))
+    def _draw_circle(self, center, radius, fill_color, outline_color):
+        left = int(round((center[0] - radius + 0.5) * self.width))
+        right = int(round((center[0] + radius + 0.5) * self.width))
+        upper = int(round((center[1] + 0.5) * self.height - radius * self.width))
+        lower = int(round((center[1] + 0.5) * self.height + radius * self.width))
         self.draw.ellipse((left, upper, right, lower), fill=fill_color, outline=outline_color)
+
 
 if __name__ == '__main__':
     vincent = VanGogh()
@@ -139,10 +147,11 @@ if __name__ == '__main__':
     for gs in list_gs:
         vincent.grid_size = gs
         for filename in vincent.get_original_images():
-            if filename[-3:] not in ['jpg', 'png']:
+            filename_base, extension = os.path.splitext(filename)
+            if extension not in ['.jpg', '.jpeg', '.png']:
                 continue
-            print('Pointillizing {} with grid size {}'.format(filename[:-4], gs))
-            img = vincent.read_img(filename)
-            vincent.pointillize_img(img)
-            filename = '{}_{}p'.format(filename[:-4], gs)
+            print(f'Pointillizing {filename_base} with grid size {gs}')
+            orig_img = vincent.read_img(filename)
+            vincent.pointillize_img(orig_img)
+            filename = f'{filename_base}_{gs}p'
             vincent.save(filename, 'pointillize')
