22260beb0041daae2826be6fd12d7be6a6d334ee
Erwin Rossen <erwin.rossen@fashion.cloud>
2022-06-26 17:56:12 +0200

    Sandbox testing

--- src/Sandbox/test.py
+++ src/Sandbox/test.py
@@ -1,7 +1,5 @@
 import os.path
-
-from PIL import Image
-from PIL.PngImagePlugin import PngImageFile
+import PIL
 
 from utils import img_dir
 
@@ -10,15 +8,10 @@ BLACK = (0, 0, 0, 0)
 TRANSPARENT = (0, 0, 0, 255)
 
 if __name__ == '__main__':
-    circle: PngImageFile = Image.open(os.path.join(img_dir(), 'circle.png'))
-    text: PngImageFile = Image.open(os.path.join(img_dir(), 'text.png'))
-
-    # Set all white pixels to transparent
-    text_pixels = [TRANSPARENT if p == WHITE else p for p in text.getdata()]
-    print([p for p in text_pixels if p not in [BLACK, WHITE, TRANSPARENT]])
-    text.putdata(text_pixels)
-    text.show()
-
-    # print(mask)
-    # circle.paste(text, None, mask=text)
-    # circle.show()
+    print(PIL.__version__)
+    width = height = 200
+    img = PIL.Image.open(os.path.join(img_dir(), 'circle.png'))
+    img.resize((width//2, height//2))
+    caption_area = PIL.Image.new('RGBA', (width, height), 'white')
+    caption_area.paste(img)
+    caption_area.show()
